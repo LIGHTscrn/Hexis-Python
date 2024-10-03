7577ff2cd2a980781e20882b3c5fe4870fb0a3a3
LIGHTscrn <pranavprasad.r500@gmail.com>
2024-10-03 22:28:10 +0530

    Printing corressponding address to pincode

--- task2-Postaltoaddress.py
+++ task2-Postaltoaddress.py
@@ -2,3 +2,8 @@ import requests
 
 pincode = input("Enter the pincode: ")
 response = requests.get(f"https://api.postalpincode.in/pincode/{pincode}")
+
+if response.json()[0]["Status"] == "Error":
+    print("Invalid Pincode")
+else:
+    print(response.json()[0]["PostOffice"][0]["Name"])
