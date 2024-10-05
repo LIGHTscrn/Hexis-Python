5e2e7487018c8c8b2e0c2ed2d26e62bbbd3324fa
LIGHTscrn <pranavprasad.r500@gmail.com>
2024-10-05 23:29:33 +0530

    Updated according to the change in requirements

--- task3/app.py
+++ task3/app.py
@@ -22,5 +22,28 @@ def dog():
     data = res.json()['message']
     return render_template("dog.html", data = data)
 
+@app.route("/pincode" , methods = ["GET" , "POST"])
+def pincode():
+    if request.method == "POST":
+        pincode = request.form.get("pincode")
+        return redirect(url_for('results', pincode=pincode))
+    return render_template("pincode.html")
+
+@app.route("/results" , methods = ["GET"])
+def results():
+    pincode = request.args.get("pincode")
+    res = requests.get(f"https://api.postalpincode.in/pincode/{pincode}")
+    data = res.json()
+    print("This is data parrrttt", data)
+    if data[0]['Status'] == "404":
+        url = "https://api.thecatapi.com/v1/images/search"
+        res = requests.get(url)
+        date = res.json()[0]['url']
+        return render_template("error.html" , date = date)
+    else:
+        address = data[0]["PostOffice"][0]["Name"]
+        print("This is the address", address)
+        return render_template("results.html" , address = address)  
+
 if __name__ == '__main__':
     app.run(debug=True)
